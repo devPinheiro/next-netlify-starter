5c86b059321f371a013d7eb241033fb8d35d39b1
devPinheiro <pinheirolaoluwa@gmail.com>
2022-03-27 17:44:24 +0100

    initial comit

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <div>
+      <Image
+        src='https://res.cloudinary.com/appnet/image/upload/v1648398672/Artboard_1_4x.png'
+        alt='Nickel logo'
+        width={320}
+        height={450}
+        layout='intrinsic'
+      />
+      {/* <h1 className='title'>{title}</h1> */}
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,22 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+<link rel="manifest" href="/site.webmanifest"/>
+        <title>Nickel NG</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header  />
+        <p className='description'>We are under construction 🚧</p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
